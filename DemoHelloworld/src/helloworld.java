4012517d1da12c68130a12bcae406a5130a4b6ff
Ubuntu <ubuntu@ip-172-31-8-127.ec2.internal>
2019-08-30 10:08:30 +0000

    hellowoworldfirstcommit

---
+++ DemoHelloworld/src/helloworld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ubuntu
+ *
+ */
+public class helloworld {
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+	
+	
+
+}
